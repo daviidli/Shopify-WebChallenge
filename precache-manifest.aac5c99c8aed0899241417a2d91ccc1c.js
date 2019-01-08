45f03fb7c1686b7a8da1ada822e84213878b2b2e
David Li <b7p0b@ugrad.cs.ubc.ca>
2019-01-08 14:38:06 -0800

    Updates

---
+++ precache-manifest.aac5c99c8aed0899241417a2d91ccc1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7442af04ef781a32194e",
+    "url": "/Shopify-WebChallenge/static/js/runtime~main.7442af04.js"
+  },
+  {
+    "revision": "688421bee2f67b6f7d99",
+    "url": "/Shopify-WebChallenge/static/js/main.688421be.chunk.js"
+  },
+  {
+    "revision": "4a6b0bac5ba0a3dbe981",
+    "url": "/Shopify-WebChallenge/static/js/1.4a6b0bac.chunk.js"
+  },
+  {
+    "revision": "688421bee2f67b6f7d99",
+    "url": "/Shopify-WebChallenge/static/css/main.01c7ed73.chunk.css"
+  },
+  {
+    "revision": "f07c8a80636ff270909f8bb73a6e444f",
+    "url": "/Shopify-WebChallenge/index.html"
+  }
+];
